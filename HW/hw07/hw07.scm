82dde44ea8321bc41e6ceef598a6e96d9432be04
TreeYD <treeyd@qq.com>
2022-09-07 17:12:00 +0800

    finish hw07

--- HW/hw07/hw07.scm
+++ HW/hw07/hw07.scm
@@ -85,7 +85,7 @@
 
 
 (define-macro (list-of map-expr for var in lst if filter-expr)
-  ;'YOUR-CODE-HERE
+  'YOUR-CODE-HERE
   `(map (lambda (,var) ,map-expr) (filter (lambda (,var) ,filter-expr) ,lst))
 )
 
